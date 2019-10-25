58a9609903c70e43c071ce7583cc7f3ed38f5d9b
jcli23 <623896069@qq.com>
2019-10-25 17:43:05 +0800

    更新

--- src/views/form/Form.vue
+++ src/views/form/Form.vue
@@ -1,6 +1,36 @@
 <template>
-  <div>
-    Form
+  <div style="background: #f0f2f5">
+    <div class="input">
+      <input type="text" placeholder="请输入你想搜索的商品信息">
+    </div>
+    <div class="table-header">
+      <div>名称</div>
+      <div>商品编号</div>
+      <div>原价</div>
+      <div>现价</div>
+      <div>操作</div>
+    </div>
+    <div v-for="(item,index) in tableData.slice(pages*(pagesNum-1),pages*pagesNum)" :key="index" class="table-label">
+      <div>{{item.NAME}}</div>
+      <div>{{item.GOODS_SERIAL_NUMBER}}</div>
+      <div>{{item.ORI_PRICE}}</div>
+      <div>{{item.PRESENT_PRICE}}</div>
+      <div class="button">
+        <el-button type="primary" @click="open"><i class="el-icon-edit"></i>修改</el-button>
+        <el-button type="danger"><i class="el-icon-delete"></i>删除</el-button>
+      </div>
+    </div>
+    <div class="page">
+      <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-sizes="[10, 20, 30, 40]"
+          :page-size="10"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="this.tableData.length ">
+      </el-pagination>     <!--分页效果-->
+    </div>
   </div>
   
 </template>
@@ -11,11 +41,45 @@
     components: {},
     props: {},
     data() {
-      return {}
+      return {
+        tableData:[],
+        currentPage:1,
+        pages:10,
+        pagesNum:1,
+      }
     },
-    methods: {},
+    methods: {
+      gettableData () {
+        this.$axios.req('api/tableData').then(res => {
+          console.log(res)
+          this.tableData = res.data//拿到数据
+        }).catch(err => {
+          console.log(err)  //报错打印
+        })
+      },
+      // 翻页事件
+      handleSizeChange(val) {
+        // console.log(`每页 ${val} 条`);
+        this.pages=val;
+      },
+      handleCurrentChange(val) {
+        // console.log(`当前页: ${val}`);
+        this.pagesNum=val
+      },
+      open() {
+        this.$alert('这是一段内容', '标题名称', {
+          confirmButtonText: '确定',
+          callback: action => {
+            this.$message({
+              type: 'info',
+              message: `action: ${ action }`
+            });
+          }
+        });
+      }
+    },
     mounted() {
-
+      this.gettableData ()
     },
     created() {
 
@@ -28,5 +92,56 @@
 </script>
 
 <style scoped lang="scss">
-
+  .input{
+    margin: 1%;
+    input{
+      font-size: 12px;
+      height: 25px;
+      width: 250px;
+      background: white;
+    }
+  }
+  .table-header{
+    border: 1px solid lightgrey;
+    height: 40px;
+    background: white;
+    display: flex;
+    margin: 0 1%;
+    div{
+      flex: 1;
+      line-height: 40px;
+      color: #444444;
+      font-size: 14px;
+      text-align: center;
+    }
+  }
+  .table-label {
+    border: 1px solid lightgrey;
+    border-top:none;
+    height: 55px;
+    background: white;
+    display: flex;
+    margin: 0 1%;
+    align-items: center;
+    div {
+      flex: 1;
+      color: #444444;
+      font-size: 14px;
+      text-align: center;
+    }
+    .button{
+      flex: 1;
+      height: 40px;
+      line-height: 55px;
+      display: flex;
+      justify-content: center;
+    }
+    &:hover{
+      background: #f0f2f5;
+    }
+  }
+  .page{
+    margin: 10px 1%;
+    background: white;
+  }
 </style>
--- src/views/home_page/Home_page.vue
+++ src/views/home_page/Home_page.vue
@@ -57,6 +57,63 @@
         <ve-histogram :data="histogramChat" :settings="chartSettings4"></ve-histogram>
       </div>
     </div>
+    <div class="bottom">
+      <div class="bottom-left">
+        <div class="table-header">
+          <div class="num">Order_No</div>
+          <div class="price">Price</div>
+          <div class="status">Status</div>
+        </div>
+        <div v-for="(item,index) in tableData" :key="index" class="table-label">
+          <div class="num1">{{item.num}}</div>
+          <div class="price1">￥{{item.price}}</div>
+          <div class="status">
+            <div v-if="item.status ===0" class="status1">pending</div>
+            <div v-if="item.status ===1" class="status2">success</div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom-center">
+        <div class="header">
+          <div style="width: 50px;height: 50px;text-align: center" @click="checkedAll">
+            <i class="el-icon-arrow-down check"></i>
+          </div>
+          <span class="right">Todo List</span>
+        </div>
+        <div v-for="(item,index) in todoList" :key="index" class="todo">
+          <div v-if="item.checked === !check" class="yuan" @click="itemClick"><i class="el-icon-check"></i></div>
+          <div v-if="item.checked === check" class="yuan" @click="itemClick"></div>
+          <div class="title">{{item.name}}</div>
+        </div>
+        <div>
+          <div class="line"></div>
+        </div>
+        <div class="bottom">
+          <div class="one">{{}}items left</div>
+          <div class="two">All</div>
+          <div class="three">Active</div>
+          <div class="four">Completed</div>
+        </div>
+      </div>
+      <div class="bottom-right">
+        <div class="bottom-right-main">
+          <img src="../../../public/images/img5.png" alt="" class="img">
+          <div class="slip">
+            <div v-for="(item,index) in slip" :key="index">
+              <div v-if="item.progress<1">
+                <div>{{item.name}}</div>
+                <el-progress :percentage="item.progress*100">
+                </el-progress>
+              </div>
+              <div v-if="item.progress===1">
+                <div>{{item.name}}</div>
+                <el-progress :percentage="100" status="success"></el-progress>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
   
 </template>
@@ -94,6 +151,11 @@
         dimension: ['date']
       }
       return {
+        check:false,
+        checkAll:false,
+        todoList:[],
+        slip:[],
+        tableData:[],
         chartData: {
           columns: ["date",'expected' , 'actual'],
           rows: [
@@ -165,12 +227,46 @@
           console.log(err)  //报错打印
         })
       },
+      getorderData () {
+        this.$axios.req('api/orderData').then(res => {
+          console.log(res)
+          this.tableData = res.data   //拿到数据
+        }).catch(err => {
+          console.log(err)  //报错打印
+        })
+      },
+      getprogress () {
+        this.$axios.req('api/progress').then(res => {
+          console.log(res)
+          this.slip = res.data   //拿到数据
+        }).catch(err => {
+          console.log(err)  //报错打印
+        })
+      },
+      gettodoList () {
+        this.$axios.req('api/todoList').then(res => {
+          console.log(res)
+          this.todoList = res.data//拿到数据
+        }).catch(err => {
+          console.log(err)  //报错打印
+        })
+      },
+      itemClick(){
+        // this.check=!this.check
+      },
+      checkedAll(){
+        this.check=!this.checkAll
+      }
+
     },
     mounted() {
       this.gethomeData ()
       this.gethomeChat ()
       this.getradarChat ()
       this.getringChat ()
+      this.getorderData()
+      this.getprogress()
+      this.gettodoList()
     },
     created() {
 
@@ -212,6 +308,182 @@
         background: white;
         flex: 1;
         margin: 20px;
+      }
+    }
+    .bottom{
+      display: flex;
+      .bottom-left{
+        width: 52%;
+        .table-header{
+          background: white;
+          display: flex;
+          height: 70px;
+          font-size: 16px;
+          align-items: center;
+          color: grey;
+          border-bottom: 1px solid lightgray;
+          .num{
+            width: 45%;
+            text-align: center;
+          }
+          .price{
+            width: 25%;
+            text-align: center;
+          }
+          .status{
+            width: 30%;
+            text-align: center;
+          }
+        }
+        .table-label{
+          background: white;
+          display: flex;
+          height: 55px;
+          font-size: 14px;
+          align-items: center;
+          color: #444444;
+          border-bottom: 1px solid lightgray;
+          .num1{
+            width: 45%;
+            text-align: center;
+          }
+          .price1{
+            width: 25%;
+            text-align: center;
+          }
+          .status{
+            width: 30%;
+            .status1{
+              border-radius: 4px;
+              background: mistyrose;
+              font-size: 12px;
+              text-align: center;
+              color: red;
+              margin: 0 auto;
+              width: 60px;
+              height: 30px;
+              line-height: 30px;
+              justify-content: center;
+            }
+            .status2{
+              border:1px solid palegreen;
+              border-radius: 4px;
+              background: paleturquoise;
+              font-size: 12px;
+              text-align: center;
+              color: forestgreen;
+              margin: 0 auto;
+              width: 60px;
+              height: 30px;
+              line-height: 30px;
+              justify-content: center;
+            }
+          }
+
+        }
+      }
+      .bottom-center{
+        background: white;
+        width: 24%;
+        margin-left: 1%;
+        .header{
+          display: flex;
+          position: relative;
+          width: 100%;
+          height: 50px;
+          border-bottom: 1px solid silver;
+          .check{
+            width: 15%;
+            line-height: 50px;
+            margin: 0 auto;
+          }
+          .right{
+            position: absolute;
+            right: 5%;
+            font-size: 16px;
+            line-height: 50px;
+          }
+        }
+        .todo{
+          display: flex;
+          height: 35px;
+          align-items: center;
+          margin-top: 3%;
+          margin-left: 3%;
+          .yuan{
+            border: 1px solid lightslategray;
+            width: 19px;
+            height: 19px;
+            border-radius: 50%;
+          }
+          .title{
+            margin-left: 10%;
+          }
+        }
+        .line{
+          border: 1px solid silver;
+          width: 80%;
+          margin: 10px 10%;
+        }
+        .bottom{
+          display: flex;
+          align-items: center;
+          height: 70px;
+          .one{
+            width: 35%;
+            font-size: 14px;
+            text-align: center;
+          }
+          .two{
+            border: 1px solid white;
+            width: 20%;
+            text-align: center;
+            height: 25px;
+            line-height: 25px;
+            font-size: 14px;
+            &:hover{
+              border: 1px solid navajowhite;
+            }
+          }
+          .three{
+            border: 1px solid white;
+            text-align: center;
+            width: 20%;
+            height: 25px;
+            line-height: 25px;
+            font-size: 14px;
+            &:hover{
+              border: 1px solid navajowhite;
+            }
+          }
+          .four{
+            border: 1px solid white;
+            text-align: center;
+            width: 25%;
+            height: 25px;
+            line-height: 25px;
+            font-size: 14px;
+            &:hover{
+              border: 1px solid navajowhite;
+            }
+          }
+        }
+      }
+      .bottom-right{
+        background: white;
+        width: 22%;
+        margin-left: 1%;
+        .bottom-right-main{
+          margin-top: 5%;
+          margin-left: 5%;
+          .img{
+            width: 100%;
+            height: 100%;
+          }
+          .slip{
+            margin-top: 50px;
+          }
+        }
       }
     }
   }
